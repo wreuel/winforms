a3fc8fe711eb4a903b463b2beb0fb2267d04d6fa
Igor Velikorossov <igor.velikorossov@microsoft.com>
2019-11-04 21:18:16 +1100

    some progress

---
+++ eng/restore-toolset.ps1
@@ -0,0 +1,6 @@
+#
+# This file should be kept in sync across https://www.github.com/dotnet/wpf and dotnet-wpf-int repos. 
+#
+
+. $PsScriptRoot\common\init-tools-native.ps1 -InstallDirectory $PSScriptRoot\..\.tools\native -GlobalJsonFile $PSScriptRoot\..\global.json
+
