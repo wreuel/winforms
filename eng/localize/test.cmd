3029ada686a14e690770b67d8aa1b25dcedefd22
Igor Velikorossov <igor.velikorossov@microsoft.com>
2021-01-25 19:26:13 +1100

    Initial OneLocBuild hook up

---
+++ eng/localize/test.cmd
@@ -0,0 +1,23 @@
+:: @echo off
+
+pushd "%~dp0"
+
+setlocal
+
+set RepoRoot=..\..
+set LocProject=%~dp0LocProject.json
+set LocalizationXLocPkgVer=2.0.1
+
+set OutDir=%~dp0out
+set NUGET_PACKAGES=C:\Users\igveliko\.nuget\packages
+set XLocPath=%NUGET_PACKAGES%\Localization.XLoc\%LocalizationXLocPkgVer%
+
+echo Running Localization...
+
+::"%XLocPath%\tools\netfx\Microsoft.Localization.XLoc.exe" /f "%LocProject%"
+dotnet "%XLocPath%\tools\netcore\Microsoft.Localization.XLoc.dll" /f "%LocProject%"
+
+echo Localization finished with exit code '%errorlevel%'.
+
+popd
+exit /b %errorlevel%
